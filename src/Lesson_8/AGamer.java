86eacd06d13a14c0eace58d670e571cc61fc72ff
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2021-07-16 11:07:38 +0300

    Add Lesson8

---
+++ src/Lesson_8/AGamer.java
@@ -0,0 +1,13 @@
+package Lesson_8;
+
+public abstract class AGamer {
+
+    // символ
+    protected String sign;
+
+    // выстрел
+    abstract boolean shot (int x, int y);
+
+    // провека победы
+    abstract boolean win();
+}
---
+++ src/Lesson_8/MainClass.java
@@ -0,0 +1,7 @@
+package Lesson_8;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MainForm gameForm = new MainForm();
+    }
+}
